cb6c945a64868b78506a0a6b93c8292b98d608dd
Alvin Siew <alvin_siew@tech.gov.sg>
2021-05-06 21:02:09 +0800

    first app

---
+++ ecr_scan/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecr"
+)
+
+func LambdaHandler() {
+	svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
+	input := &ecr.DescribeRepositoriesInput{}
+
+	result, err := svc.DescribeRepositories(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecr.ErrCodeServerException:
+				fmt.Println(ecr.ErrCodeServerException, aerr.Error())
+			case ecr.ErrCodeInvalidParameterException:
+				fmt.Println(ecr.ErrCodeInvalidParameterException, aerr.Error())
+			case ecr.ErrCodeRepositoryNotFoundException:
+				fmt.Println(ecr.ErrCodeRepositoryNotFoundException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
+
+func ecrImages() *ecr.ListImagesOutput {
+	svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
+	input := &ecr.ListImagesInput{
+		RepositoryName: aws.String("dbserver"),
+	}
+
+	result, err := svc.ListImages(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ecr.ErrCodeServerException:
+				fmt.Println(ecr.ErrCodeServerException, aerr.Error())
+			case ecr.ErrCodeInvalidParameterException:
+				fmt.Println(ecr.ErrCodeInvalidParameterException, aerr.Error())
+			case ecr.ErrCodeRepositoryNotFoundException:
+				fmt.Println(ecr.ErrCodeRepositoryNotFoundException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		//return
+	}
+
+	//fmt.Println("result = ", reflect.TypeOf(result))
+	//fmt.Println(result)
+
+	return result
+}
+
+func main() {
+	//ecrImages()
+
+	//abc := ecrImages()
+	lambda.Start(LambdaHandler)
+
+	//ecrRepositories()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module ecr_scan
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.23.0 // indirect
+	github.com/aws/aws-sdk-go v1.38.34 // indirect
+	github.com/go-delve/delve v1.6.0 // indirect
+)
