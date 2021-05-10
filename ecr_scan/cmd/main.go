68340a9e3deb64dcc36d27b5b702cb8b87ef93d5
Alvin Siew <alvin_siew@tech.gov.sg>
2021-05-10 10:42:45 +0800

    in progress

--- ecr_scan/cmd/main.go
+++ ecr_scan/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/aws/aws-lambda-go/lambda"
+	"sort"
 
 	//"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws"
@@ -11,16 +11,10 @@ import (
 	"github.com/aws/aws-sdk-go/service/ecr"
 )
 
-//type ScanSpec struct {
-//	ImageID ImageID `json:"imageId"`
-//	RegistryID string `json:"registry"`
-//	Repository string `json:"repository"`
-//}
-
 type ScanSpec struct {
-	//ImageID ImageIdentifier `locationName:"imageId" type:"structure" required:"true"`
-	RegistryID string `locationName:"registryId" type:"string"`
-	Repository string `locationName:"repositoryName" min:"2" type:"string" required:"true"`
+	ImageID    imageID `locationName:"imageId" type:"structure" required:"true"`
+	RegistryID string  `locationName:"registryId" type:"string"`
+	Repository string  `locationName:"repositoryName" min:"2" type:"string" required:"true"`
 }
 
 type imageID struct {
@@ -28,70 +22,45 @@ type imageID struct {
 	ImageTag    string `locationName:"imageDigest" type:"string"`
 }
 
-//type ImageIdentifier struct {
-//
-//	// The sha256 digest of the image manifest.
-//	ImageDigest string `locationName:"imageDigest" type:"string"`
-//
-//	// The tag used for the image.
-//	ImageTag string `locationName:"imageTag" min:"1" type:"string"`
-//	// contains filtered or unexported fields
-//}
-
-func ecrStartScan() {
+func EcrNewSession() *ecr.ECR {
 	svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
 
-	//ImageDigest := "sha256:80d8b356e087631ac21bdf5aa51e4917bef73baa4e398a89a57c9e26f2fec342"
-	//ImageTag := "latest"
+	return svc
+}
 
-	specImage := &imageID{
+func HandlerStartScan(svc *ecr.ECR) {
+	specImage := imageID{
 		ImageDigest: "sha256:1e1a915f208cab016a981212218d465053572007b9122b5e149d16830e33751b",
-		ImageTag:    "latest",
+		ImageTag:    "7-2.7.1-12.22.1",
 	}
 
-	id := aws.String(specImage.ImageDigest)
-	it := aws.String(specImage.ImageTag)
-
-	//fmt.Println(ImageDigest)
-
 	spec := &ScanSpec{
-		//ImageID: imageSpec,
+		ImageID:    specImage,
 		RegistryID: "355716222559",
 		Repository: "centos-ruby-node",
 	}
 
-	//test := &ecr.ImageIdentifier{ImageTag: id, ImageDigest: it}
-	//svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
 	input := &ecr.StartImageScanInput{
 		ImageId: &ecr.ImageIdentifier{
-			ImageTag:    it,
-			ImageDigest: id,
+			ImageTag:    &spec.ImageID.ImageTag,
+			ImageDigest: &spec.ImageID.ImageDigest,
 		},
 		RepositoryName: &spec.Repository,
 		RegistryId:     &spec.RegistryID,
 	}
 
-	//input := &ecr.BatchDeleteImageInput{
-	//	ImageIds: []*ecr.ImageIdentifier{
-	//		{
-	//			ImageTag: aws.String("precise"),
-	//		},
-	//	},
-	//	RepositoryName: aws.String("ubuntu"),
-	//}
-
 	result, err := svc.StartImageScan(input)
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok {
-			switch aerr.Code() {
+		if aer, ok := err.(awserr.Error); ok {
+			switch aer.Code() {
 			case ecr.ErrCodeServerException:
-				fmt.Println(ecr.ErrCodeServerException, aerr.Error())
+				fmt.Println(ecr.ErrCodeServerException, aer.Error())
 			case ecr.ErrCodeInvalidParameterException:
-				fmt.Println(ecr.ErrCodeInvalidParameterException, aerr.Error())
+				fmt.Println(ecr.ErrCodeInvalidParameterException, aer.Error())
 			case ecr.ErrCodeRepositoryNotFoundException:
-				fmt.Println(ecr.ErrCodeRepositoryNotFoundException, aerr.Error())
+				fmt.Println(ecr.ErrCodeRepositoryNotFoundException, aer.Error())
 			default:
-				fmt.Println(aerr.Error())
+				fmt.Println(aer.Error())
 			}
 		} else {
 			// Print the error, cast err to awserr.Error to get the Code and
@@ -103,8 +72,7 @@ func ecrStartScan() {
 	fmt.Println(result)
 }
 
-func LambdaHandler() {
-	svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
+func HandlerListRepo(svc *ecr.ECR) []string {
 	input := &ecr.DescribeRepositoriesInput{}
 
 	result, err := svc.DescribeRepositories(input)
@@ -125,16 +93,22 @@ func LambdaHandler() {
 			// Message from an error.
 			fmt.Println(err.Error())
 		}
-		return
+		//return
 	}
 
-	fmt.Println(result)
+	var repoName []string
+
+	for _, r := range result.Repositories {
+		repoName = append(repoName, *r.RepositoryName)
+	}
+
+	return repoName
 }
 
-func ecrImages() *ecr.ListImagesOutput {
-	svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
+func HandleListImage(svc *ecr.ECR, rn string) *ecr.ListImagesOutput {
+	//svc := ecr.New(session.Must(session.NewSession()), aws.NewConfig().WithRegion("ap-southeast-1"))
 	input := &ecr.ListImagesInput{
-		RepositoryName: aws.String("dbserver"),
+		RepositoryName: aws.String(rn),
 	}
 
 	result, err := svc.ListImages(input)
@@ -165,11 +139,13 @@ func ecrImages() *ecr.ListImagesOutput {
 }
 
 func main() {
-	//ecrImages()
+	//lambda.Start(HandlerStartScan)
+	svc := EcrNewSession()
+	//HandleListImage(svc)
+	abc := HandlerListRepo(svc)
 
-	//abc := ecrImages()
-	lambda.Start(ecrStartScan)
+	for _, r := range abc {
+		fmt.Println(sort.Strings(string(HandleListImage(svc, r))))
+	}
 
-	//ecrRepositories()
-	//ecrStartScan()
 }
